833375e3c138a3009277fd895be54c380049dd04
Rafael Marques <rafael.marques-98@hotmail.com>
2024-07-23 21:10:05 -0300

    feat: criando casos de uso de auth

---
+++ src/use-cases/authenticate-org/authenticate-org-use-case.ts
@@ -0,0 +1,70 @@
+import { Organization } from "@/interfaces/Organization";
+import { User } from "@/interfaces/User";
+import { OrganizationsRepository } from "@/repostiories/organizations-repository";
+import { UserRepository } from "@/repostiories/user-repository";
+import { compare } from "bcryptjs";
+import { InvalidCredentialsError } from "../errors/invalid-credentials-error";
+import { UserNotFoundError } from "../errors/user-not-found-error";
+
+interface AuthenticateUseCaseRequest {
+  email: string;
+  password: string;
+}
+interface AuthenticateUseCaseResponse {
+  organization: Required<Organization>;
+}
+
+export class AuthenticateUseCase {
+  constructor(
+    private organizationRepository: OrganizationsRepository,
+    private userRepository: UserRepository
+  ) {}
+
+  private async findOrganizationByEmail(email: string): Promise<Organization> {
+    const organization = await this.organizationRepository.findByEmail(email);
+    if (!organization) {
+      throw new InvalidCredentialsError();
+    }
+
+    return organization;
+  }
+
+  private async findUserById(id: string): Promise<User> {
+    const user = await this.userRepository.findById(id);
+    if (!user) {
+      throw new UserNotFoundError();
+    }
+
+    return user;
+  }
+
+  private async validatePassword({
+    password,
+    passwordHash,
+  }: {
+    password: string;
+    passwordHash: string;
+  }): Promise<void> {
+    const isValid = await compare(password, passwordHash);
+
+    if (!isValid) {
+      throw new InvalidCredentialsError();
+    }
+  }
+
+  async execute(
+    data: AuthenticateUseCaseRequest
+  ): Promise<AuthenticateUseCaseResponse> {
+    const organization = await this.findOrganizationByEmail(data.email);
+    const user = await this.findUserById(organization.user_id);
+
+    await this.validatePassword({
+      password: data.password,
+      passwordHash: user.password_hash,
+    });
+
+    return {
+      organization,
+    };
+  }
+}
---
+++ src/use-cases/authenticate-org/authenticate-org.spec.ts
@@ -0,0 +1,114 @@
+import { InMemoryOrganizationsRepository } from "@/repostiories/in-memory/in-memory-organizations-repository";
+import { InMemoryUserRepository } from "@/repostiories/in-memory/in-memory-user-repository";
+import { OrganizationsRepository } from "@/repostiories/organizations-repository";
+import { UserRepository } from "@/repostiories/user-repository";
+import { hash, hashSync } from "bcryptjs";
+import { beforeEach, describe, expect, it } from "vitest";
+import { InvalidCredentialsError } from "../errors/invalid-credentials-error";
+import { AuthenticateUseCase } from "./authenticate-org-use-case";
+
+let sut: AuthenticateUseCase;
+let userRepository: UserRepository;
+let organizationRepository: OrganizationsRepository;
+
+describe("Authenticate use case", () => {
+  beforeEach(() => {
+    organizationRepository = new InMemoryOrganizationsRepository();
+    userRepository = new InMemoryUserRepository();
+    sut = new AuthenticateUseCase(organizationRepository, userRepository);
+  });
+
+  it("Should be able to authenticate with a organization", async () => {
+    const user = await userRepository.register({
+      email: "john.doe@example.com",
+      password_hash: hashSync("password123", 6),
+      id: "test123",
+    });
+
+    await organizationRepository.register({
+      address: {
+        street: "Rua 123",
+        number: "123",
+        city: "Cidade Teste",
+        state: "SP",
+        cep: "",
+      },
+      email: user.email,
+      responsible: "John Snow",
+      whatsapp: "999999999",
+      id: "test123",
+      user_id: user.id,
+    });
+
+    const { organization } = await sut.execute({
+      email: "john.doe@example.com",
+      password: "password123",
+    });
+
+    expect(organization).toEqual(
+      expect.objectContaining({
+        id: "test123",
+      })
+    );
+  });
+
+  it("Should not be able to authenticate with email incorrect", async () => {
+    const user = await userRepository.register({
+      email: "john123@example.com",
+      password_hash: hashSync("password123", 6),
+      id: "test123",
+    });
+
+    await organizationRepository.register({
+      address: {
+        street: "Rua 123",
+        number: "123",
+        city: "Cidade Teste",
+        state: "SP",
+        cep: "",
+      },
+      email: user.email,
+      responsible: "John Snow",
+      whatsapp: "999999999",
+      id: "test123",
+      user_id: user.id,
+    });
+
+    await expect(
+      sut.execute({
+        email: "john.doe@example.com",
+        password: "password123",
+      })
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+
+  it("Should not be able to authenticate with password incorrect", async () => {
+    const user = await userRepository.register({
+      email: "john123@example.com",
+      password_hash: await hash("password123", 6),
+      id: "test123",
+    });
+
+    await organizationRepository.register({
+      address: {
+        street: "Rua 123",
+        number: "123",
+        city: "Cidade Teste",
+        state: "SP",
+        cep: "",
+      },
+      email: user.email,
+      responsible: "John Snow",
+      whatsapp: "999999999",
+      id: "test123",
+      user_id: user.id,
+    });
+
+    await expect(
+      sut.execute({
+        email: "john123@example.com",
+        password: "1234",
+      })
+    ).rejects.toBeInstanceOf(InvalidCredentialsError);
+  });
+});
